e277c8e3d5e5ebd4c0f0192e0d4a2249358cf3a9
djj <djj@tungee.com>
2018-02-09 21:58:25 +0800

    clean up

--- core/target_tree.py
+++ core/target_tree.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    __init__
+    target_tree
     ~~~~~~~
 
     Module description here.
 
     :author: djj
-    :copyright: (c) 2017, Tungee
-    :date created: 2017-12-23
+    :copyright: (c) 2018, Tungee
+    :date created: 2018-02-09
     :python version: 2.7
 """
